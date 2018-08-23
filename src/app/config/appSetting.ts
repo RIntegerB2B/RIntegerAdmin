3a388d5f0bde72873ceed13eb723f31a6055b9f8
Vikasini <vikasininithi@gmail.com>
2018-08-23 20:41:15 +0530

    changes in status

--- src/app/config/appSetting.ts
+++ src/app/config/appSetting.ts
@@ -1,7 +1,7 @@
 export const AppSetting: AppSettingType = {
     awsServiceUrl: 'http://ec2-13-126-16-163.ap-south-1.compute.amazonaws.com:3001/',
     local3010BuyerServiceUrl: 'http://localhost:3012/',
- //  serviceUrl: 'http://localhost:3012/',
-      serviceUrl: 'https://rinteger.com/adminservice/',
+  // serviceUrl: 'http://localhost:3012/',
+       serviceUrl: 'https://rinteger.com/adminservice/',
     imageUrl: 'https://rinteger.com/assets/images/logohomepage.jpg'
 };
