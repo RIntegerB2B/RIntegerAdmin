300e06d9caac84d98886fa6323e66917e66dcd0c
Vikasini <vikasininithi@gmail.com>
2018-09-08 19:45:37 +0530

    changes in model-management

--- src/app/config/appSetting.ts
+++ src/app/config/appSetting.ts
@@ -2,13 +2,7 @@ import { environment } from '../../environments/environment';
 export const AppSetting: AppSettingType = {
     awsServiceUrl: 'http://ec2-13-126-16-163.ap-south-1.compute.amazonaws.com:3001/',
     local3010BuyerServiceUrl: 'http://localhost:3012/',
-<<<<<<< Updated upstream
     //  serviceUrl: 'http://localhost:3012/',
     serviceUrl: environment.serviceUrl,
     imageUrl: environment.imageUrl
-=======
-     //  serviceUrl: 'http://localhost:3012/',
-       serviceUrl: 'https://rinteger.com/adminservice/',
-    imageUrl: 'https://rinteger.com/assets/images/logohomepage.jpg'
->>>>>>> Stashed changes
 };
