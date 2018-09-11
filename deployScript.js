4852cd6f3100e28351a8f6838f356c0820fdfe79
Preveen Chandran <Preveen_Chandran@Dell.com>
2018-09-11 22:47:38 +0530

    Deploy

--- deployScript.js
+++ deployScript.js
@@ -1,6 +1,5 @@
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function () {
-  console.log('Ready State: ' + this.readyState + 'Status: ' + this.status + 'Response: ' + this.responseText);
-};
-xhttp.open("GET", "http://52.66.167.224:3020/rintegeradmin", true);
-xhttp.send();
+var request = require('request');
+request('http://52.66.167.224:3020/rintegeradmin', function (error, response, body) {
+  console.log('error:', error);
+  console.log('body:', body);
+});
