6d32235ea5803542e847b13c96f12ae623dea8e4
ripsildeveloper <ripsildeveloper3@gmail.com>
2019-03-22 18:44:04 +0530

    changes in allbooking

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   serviceUrl: 'https://rinteger.com/adminservice/',
-  imageUrl: 'https://rinteger.com/assets/images/logohomepage.jpg'
+  imageUrl: 'https://rinteger.com/assets/images/logohomepage.jpg',
+  serviceOperation: 'https://jrdrbr8zte.execute-api.ap-south-1.amazonaws.com/latest/'
 };
